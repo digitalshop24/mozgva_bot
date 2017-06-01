24db2f2b6e3004bca5b8e2750419d06b6f610f64
Sergey <biginf.com@gmail.com>
2017-06-01 16:17:02 +0300

    second commit

---
+++ main.rb
@@ -0,0 +1,27 @@
+# coding: UTF-8
+
+require 'telegram/bot'
+
+TOKEN = '390226364:AAG_hpGzIrG7VIvWAOMJjYmsZMUNgsbUyxQ'
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+	bot.listen do |message|
+		case message.text
+		when '/start', '/start start', 'Привет', 'Здраствуйте', 'привет'
+			bot.api.send_message(
+				chat_id: message.chat.id,
+				text: "Привет-привет #{message.from.first_name}"
+				)
+		when '/help'
+			bot.api.send_message(
+				chat_id: message.chat.id,
+				text: "Я могу записать тебя на игру. Только мне надо узнать из какого ты города."
+				)	
+		else
+			bot.api.send_message(
+				chat_id: message.chat.id,
+				text: "#{message.from.first_name} не понимаю о чем ты"
+				)
+		end
+	end
+end
